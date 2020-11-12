d567a7f682b3dd76ec5ea8f2f362ab425d9d2e72
hexyoungs <chuxdesign@hotmail.com>
2020-11-12 15:01:21 +0800

    add lc 49, 59

---
+++ lc/49.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    vector<vector<string>> ret;
+    unordered_map<string, vector<string>> m;
+
+    for (int i = 0; i < strs.size(); i++) {
+      char hash[26] = {'a'};
+      for (auto c : strs[i]) {
+        hash[c - 'a']++;
+      }
+      string s(hash, hash + 26);
+      m[s].push_back(strs[i]);
+    }
+
+    for (auto it = m.begin(); it != m.end(); it++) {
+      ret.push_back(it->second);
+    }
+    return ret;
+  }
+};
+
+int main() { return 0; }
---
+++ lc/59.cc
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  enum DIR { RIGHT, DOWN, LEFT, UP };
+  vector<vector<int>> generateMatrix(int n) {
+    vector<vector<int>> ret;
+    for (int i = 0; i < n; i++) {
+      vector<int> row(n, 0);
+      ret.push_back(row);
+    }
+    int x = 0;
+    int y = 0;
+    int num = 1;
+    int w = n - 1;
+    int h = n - 1;
+    int l = 0;
+    int t = 0;
+
+    enum DIR dir = RIGHT;
+    while (x >= l && x <= w && y >= t && y <= h) {
+      // cout << x << ", " << y << "    num:" << num << endl;
+      ret[y][x] = num;
+      num++;
+      if (num > n * n) break;
+      switch (dir) {
+        case RIGHT: {
+          if (x == w) {
+            dir = DOWN;
+            y++;
+            t++;
+          } else {
+            x++;
+          }
+          break;
+        }
+        case DOWN: {
+          if (y == h) {
+            dir = LEFT;
+            x--;
+            w--;
+          } else {
+            y++;
+          }
+          break;
+        }
+        case LEFT: {
+          if (x == l) {
+            dir = UP;
+            y--;
+            h--;
+          } else {
+            x--;
+          }
+          break;
+        }
+        case UP: {
+          if (y == t) {
+            dir = RIGHT;
+            x++;
+            l++;
+          } else {
+            y--;
+          }
+          break;
+        }
+      }
+      // cout << dir << ": " << x << ", " << y << endl;
+      // cout << "x range: [" << l << ", " << w << "]" << endl;
+      // cout << "y range: [" << t << ", " << h << "]" << endl;
+    }
+    return ret;
+  }
+};
+
+int main() { return 0; }
