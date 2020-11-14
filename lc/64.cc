f1c12c34763cae3b1b229830b094ce1f42feeb19
hexyoungs <chuxdesign@hotmail.com>
2020-11-14 18:15:22 +0800

    add lc64

---
+++ lc/64.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+ public:
+  int minPathSum(vector<vector<int>>& grid) {
+    int m = grid.size();
+    int n = grid[0].size();
+    vector<int> dp(m, 0);
+    dp[m - 1] = grid[m - 1][n - 1];
+
+    for (int y = m - 2; y >= 0; y--) dp[y] = dp[y + 1] + grid[y][n - 1];
+
+    for (int x = n - 2; x >= 0; x--) {
+      dp[m - 1] += grid[m - 1][x];
+      for (int y = m - 2; y >= 0; y--) {
+        dp[y] = min(dp[y], dp[y + 1]) + grid[y][x];
+      }
+    }
+
+    return dp[0];
+  }
+};
+
+int main() { return 0; }
