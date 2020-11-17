ab3d1d7523cc60e3c78fe02674e5c83e16b0ddf9
hexyoungs <chuxdesign@hotmail.com>
2020-11-17 15:15:55 +0800

    feat: implement search method

--- datastructure/skiplist/main.c
+++ datastructure/skiplist/main.c
@@ -1,12 +1,23 @@
+#include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "skiplist.h"
 
 int main() {
   skiplist *list = skiplist_new();
-  for (int i = 0; i < 100; i++) {
-    int n = rand() & 0xFF;
+  for (int i = 1; i < 100; i++) {
+    // int n = rand() & 0xFF;
+    int n = i;
     skiplist_insert(list, i, n);
+  }
+
+  for (int i = 1; i < 100; i++) {
+    const skipnode *node = skiplist_find(list, i);
+    assert(node != NULL);
+    assert(node->key == i);
+    assert(node->val == i);
+    printf("found node: (%d, %d)\n", node->key, node->val);
   }
   skiplist_destroy(list);
   return 0;
--- datastructure/skiplist/skiplist.h
+++ datastructure/skiplist/skiplist.h
@@ -1,7 +1,12 @@
 #ifndef _SKIPLIST_H
 #define _SKIPLIST_H
+#include <stddef.h>
+#include <stdlib.h>
 
 #define MAX_LEVEL 32
+#define LEVEL_PROB 0.25
+#define container_of(ptr, type, member) \
+  (type *)((char *)(ptr)-offsetof(type, member))
 
 typedef struct skiplink {
   struct skiplink *prev;
@@ -21,9 +26,123 @@ typedef struct skipnode {
   skiplink levels[0];
 } skipnode;
 
-skiplist *skiplist_new();
-void skiplist_destroy(skiplist *list);
-skipnode *skiplist_insert(skiplist *list, int k, int v);
-void skiplist_remove(skiplist *list, int k);
+//////// static
+static void skipnode_destroy(skipnode *node) { free(node); }
+
+static int random_level() {
+  int level = 1;
+  while ((rand() & 0xFFFF) < (LEVEL_PROB * 0xFFFF)) level++;
+  return level;
+}
+
+/////// static end
+
+skiplist *skiplist_new() {
+  skiplist *ret = (skiplist *)malloc(sizeof(skiplist));
+  if (ret) {
+    ret->length = 0;
+    ret->level = 1;
+    for (int i = 0; i < sizeof(ret->entries) / sizeof(ret->entries[0]); i++) {
+      ret->entries[i].prev = &ret->entries[i];
+      ret->entries[i].next = &ret->entries[i];
+    }
+  }
+
+  return ret;
+}
+
+void skiplist_destroy(skiplist *list) {
+  skiplink *next;
+  skiplink *pos = list->entries[0].next;
+  skiplink *end = &list->entries[0];
+
+  for (next = pos->next; pos != end; pos = next, next = next->next) {
+    skipnode *node = container_of(pos, skipnode, levels[0]);
+    skipnode_destroy(node);
+  }
+  free(list);
+}
+
+skipnode *skiplist_insert(skiplist *list, int k, int v) {
+  int level = random_level();
+  if (level > list->level) {
+    list->level = level;
+  }
+
+  skipnode *ptr =
+      (skipnode *)malloc(sizeof(skipnode) + sizeof(skiplink) * level);
+  if (ptr) {
+    ptr->key = k;
+    ptr->val = v;
+
+    // top-down search
+    for (int i = list->level - 1; i >= 0; i--) {
+      skiplink *entry = list->entries[i].next;
+      skiplink *right = &list->entries[i];
+
+      while (entry != right) {
+        skipnode *node = container_of(entry, skipnode, levels[i]);
+        if (node->key < k) {
+          entry = entry->next;
+        } else {
+          right = &node->levels[i];
+          break;
+        }
+      }
+
+      if (i < level) {
+        skiplink *left = right->prev;
+        ptr->levels[i].next = right;
+        ptr->levels[i].prev = left;
+        right->prev = &ptr->levels[i];
+        left->next = &ptr->levels[i];
+      }
+    }
+    list->length++;
+  }
+  return ptr;
+}
+
+void skiplist_remove(skiplist *list, int k) {
+  // very similar to insertion, need to add extra check for list's level
+  // this will delete all node with key: k
+  // from top to down
+  for (int i = list->level - 1; i >= 0; i--) {
+    skiplink *entry = list->entries[i].next;
+    skiplink *right = &list->entries[i];
+    while (entry != right) {
+      skipnode *node = container_of(entry, skipnode, levels[i]);
+      if (node->key == k) {
+        // delete
+        // TODO
+      } else if (node->key < k) {
+        entry = entry->next;
+      } else {
+        right = &node->levels[i];
+        break;
+      }
+    }
+  }
+}
+
+const skipnode *skiplist_find(skiplist *list, int k) {
+  // will return first node with key: k
+  for (int i = list->level - 1; i >= 0; i--) {
+    skiplink *entry = list->entries[i].next;
+    skiplink *right = &list->entries[i];
+    while (entry != right) {
+      skipnode *node = container_of(entry, skipnode, levels[i]);
+      if (node->key == k) {
+        return node;
+      } else if (node->key < k) {
+        entry = entry->next;
+      } else {
+        right = &node->levels[i];
+        break;
+      }
+    }
+  }
+  return NULL;
+}
 
 #endif
